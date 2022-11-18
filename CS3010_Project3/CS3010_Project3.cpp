14ac394fec1536fb145498e33fb18ae3ab78ee77
Nub456 <benjaminliu456@gmail.com>
2022-11-18 13:38:46 -0800

    Finished Secant and added comments

--- CS3010_Project3/CS3010_Project3.cpp
+++ CS3010_Project3/CS3010_Project3.cpp
@@ -3,6 +3,9 @@ Name: Benjamin Liu
 Class: CS3010
 Assignment: Programmin Project 3
 Due Date: 11/19/2022
+Notes: All algorithms used was provided from the textbook. Some slight modifications were made to those algorithms, mainly for displaying data.
+       For the data displayed in a table format, they will start being misalgined when the values start getting to big (i.e. as they get more digits)
+       but they should still be in the correct order regardless. So figuring out what data is what should still not be an issue in that case.
 */
 
 #include <iostream>
@@ -25,26 +28,61 @@ int main()
     int max = 100;
     string func1 = "2x^3 - 11.7x^2 + 17.7x - 5";
     string func2 = "x + 10 - xcosh(50/x)";
-    double a, b, x;
+    double a, b, x, err = 0.01, eps = 0.0000001;
 
+    //Finding the roots of function 2x^3 - 11.7x^2 + 17.7x - 5 
     cout << "---Bisection Method of f(x) = " << func1 << "---\n";
     cout << "Enter the a value: ";
     cin >> a;
     cout << "Enter the b value: ";
     cin >> b;
-    bisection(1, a, b, max, 0.01);
+    bisection(1, a, b, max, err);
 
     cout << "\n---False Position of f(x) = " << func1 << "---\n";
     cout << "Enter the a value: ";
     cin >> a;
     cout << "Enter the b value: ";
     cin >> b;
-    falsePos(1, a, b, max, 0.01);
+    falsePos(1, a, b, max, err);
 
     cout << "\n---Newton Raphson of f(x) = " << func1 << "---\n";
     cout << "Enter the x value: ";
     cin >> x;
-    newtonRaphson(1, x, max, 0.01, 0.0000001);
+    newtonRaphson(1, x, max, err, eps);
+
+    cout << "\n---Secant Method of f(x) = " << func1 << "---\n";
+    cout << "Enter the x1 value: ";
+    cin >> a;
+    cout << "Enter the x2 value: ";
+    cin >> b;
+    secant(1, a, b, max, err);
+
+    //Finding the roots of function x + 10 - xcosh(50/x) 
+    cout << "\n---Bisection Method of f(x) = " << func2 << "---\n";
+    cout << "Enter the a value: ";
+    cin >> a;
+    cout << "Enter the b value: ";
+    cin >> b;
+    bisection(2, a, b, max, err);
+
+    cout << "\n---False Position of f(x) = " << func2 << "---\n";
+    cout << "Enter the a value: ";
+    cin >> a;
+    cout << "Enter the b value: ";
+    cin >> b;
+    falsePos(2, a, b, max, err);
+
+    cout << "\n---Newton Raphson of f(x) = " << func2 << "---\n";
+    cout << "Enter the x value: ";
+    cin >> x;
+    newtonRaphson(2, x, max, err, eps);
+
+    cout << "\n---Secant Method of f(x) = " << func2 << "---\n";
+    cout << "Enter the x1 value: ";
+    cin >> a;
+    cout << "Enter the x2 value: ";
+    cin >> b;
+    secant(2, a, b, max, err);
 
     return 0;
 }
@@ -57,7 +95,7 @@ double f1(double x)
 //The derivative of f1
 double df1(double x)
 {
-    return ((6 * pow(x, 2)) - (23.4 * x) + 11.7);
+    return ((6 * pow(x, 2)) - (23.4 * x) + 17.7);
 }
 //The function f(x) = x + 10 - xcosh(50/x)
 double f2(double x)
@@ -73,7 +111,7 @@ double df2(double x)
 void bisection(int func, double a, double b, int max, double err)
 {
     double fa = 0, fb = 0, c = 0, fc = 0;
-    if (func == 1)
+    if (func == 1) //Since the two functions can be hard coded. func will serve as a flag variable to denote which function should be used in the calculations
     {
         fa = f1(a);
         fb = f1(b);
@@ -83,13 +121,17 @@ void bisection(int func, double a, double b, int max, double err)
         fa = f2(a);
         fb = f2(b);
     }
-
+    //Displaying table format 
     cout << "  n  " << setw(8) << "a  " << setw(10) << "f(a)  " << setw(8) << "b  " << setw(10) << "f(b)  " << setw(8) << "c  " << setw(10) << "f(c)  " << setw(7) << "err" << endl;
     cout << "---------------------------------------------------------------------------------------\n";
     if ((fa < 0 && fb < 0) || (fa >= 0 && fb >= 0))
     {
         //End function since fa and fb have the same sign
-        cout << setw(3) << 0 << setw(5) << right << a << setprecision(4) << setw(8) << fa << setw(5) << b << setw(8) << fb << endl;
+        cout << right << setw(3) << 0 << "  ";
+        cout << right << setfill(' ') << setw(6) << fixed << showpoint << setprecision(4) << a << "  ";
+        cout << right << setw(8) << fixed << showpoint << setprecision(4) << fa << "  ";
+        cout << right << setw(6) << fixed << showpoint << setprecision(4) << b << "  ";
+        cout << right << setw(8) << fixed << showpoint << setprecision(4) << fb << "  ";
         cout << "Function has same signs at a and b" << endl;
         return;
     }
@@ -135,6 +177,7 @@ void bisection(int func, double a, double b, int max, double err)
             fb = fc;
         }
     }
+    cout << "Max iterations reached. Either the function diverges or converges very slowly at these points\n";
 }
 
 void falsePos(int func, double a, double b, int max, double err)
@@ -156,14 +199,18 @@ void falsePos(int func, double a, double b, int max, double err)
     if ((fa < 0 && fb < 0) || (fa >= 0 && fb >= 0))
     {
         //End function since fa and fb have the same sign
-        cout << setw(3) << 0 << setw(5) << right << a << setprecision(4) << setw(8) << fa << setw(5) << b << setw(8) << fb << endl;
+        cout << right << setw(3) << 0 << "  ";
+        cout << right << setfill(' ') << setw(6) << fixed << showpoint << setprecision(4) << a << "  ";
+        cout << right << setw(8) << fixed << showpoint << setprecision(4) << fa << "  ";
+        cout << right << setw(6) << fixed << showpoint << setprecision(4) << b << "  ";
+        cout << right << setw(8) << fixed << showpoint << setprecision(4) << fb << "  ";
         cout << "Function has same signs at a and b" << endl;
         return;
     }
 
     for (int i = 1; i <= max; i++)
     {
-        if (i != 0)
+        if (i != 1) //Can't calculatee error on first iteration since there are no previous c values before it
         {
             prevc = c;
         }
@@ -178,7 +225,7 @@ void falsePos(int func, double a, double b, int max, double err)
             fc = f2(c);
         }
 
-        if (i != 0)
+        if (i != 1)
         {
             error = (c - prevc) / c;
         }
@@ -190,7 +237,7 @@ void falsePos(int func, double a, double b, int max, double err)
         cout << right << setw(8) << fixed << showpoint << setprecision(4) << fb << "  ";
         cout << right << setw(6) << fixed << showpoint << setprecision(4) << c << "  ";
         cout << right << setw(8) << fixed << showpoint << setprecision(4) << fc << "  ";
-        if (i != 0)
+        if (i != 1)
         {
             cout << right << setw(9) << setprecision(10) << abs(error) << endl;
         }
@@ -217,6 +264,7 @@ void falsePos(int func, double a, double b, int max, double err)
             fb = fc;
         }
     }
+    cout << "Max iterations reached. Either the function diverges or converges very slowly at these points\n";
 }
 
 void newtonRaphson(int func, double x, int max, double err, double eps)
@@ -251,7 +299,7 @@ void newtonRaphson(int func, double x, int max, double err, double eps)
         }
 
         d = fx / fp;
-        xn -= d;
+        xn -= d; //What x will be in the next iteration
         //Print table
         cout << right << setw(3) << i << "  ";
         cout << right << setfill(' ') << setw(6) << fixed << showpoint << setprecision(4) << x << "  ";
@@ -260,13 +308,16 @@ void newtonRaphson(int func, double x, int max, double err, double eps)
         cout << right << setw(6) << fixed << showpoint << setprecision(4) << d << "  ";
         cout << right << setw(8) << fixed << showpoint << setprecision(4) << xn << "  ";
         cout << right << setw(9) << setprecision(10) << abs(d) << endl;
-        x = xn;
+        
         if (abs(d) < err)
         {
             cout << "Converged at iteration " << i << ". Root is approxmiately " << x << endl;
             return;
         }
+
+        x = xn; //Set x to its next value for the following iteration
     }
+    cout << "Max iterations reached. Either the function diverges or converges very slowly at this point\n";
 }
 
 void secant(int func, double a, double b, int max, double err)
@@ -294,8 +345,14 @@ void secant(int func, double a, double b, int max, double err)
         fb = temp;
     }
 
-    //Print stuff
-    cout << "  n  " << setw(8) << "x  " << setw(10) << "f(x)  " << setw(10) << "next x  " << setw(7) << "err" << endl;
+    //Print table and the first 2 inputted values
+    cout << "  n  " << setw(8) << "x  " << setw(10) << "f(x)  " << setw(7) << "err" << endl;
+    cout << right << setw(3) << 1 << "  ";
+    cout << right << setfill(' ') << setw(6) << fixed << showpoint << setprecision(4) << a << "  ";
+    cout << right << setw(8) << fixed << showpoint << setprecision(4) << fa << endl;
+    cout << right << setw(3) << 2 << "  ";
+    cout << right << setfill(' ') << setw(6) << fixed << showpoint << setprecision(4) << b << "  ";
+    cout << right << setw(8) << fixed << showpoint << setprecision(4) << fb << endl;
     for (int i = 3; i <= max; i++)
     {
         if (abs(fa) > abs(fb))
@@ -316,7 +373,7 @@ void secant(int func, double a, double b, int max, double err)
 
         if (abs(d) < err)
         {
-            cout << "Converged" << endl;
+            cout << "Converged at iteration " << i-1 << ". Root is approximately " << a << endl;
             return;
         }
 
@@ -329,6 +386,12 @@ void secant(int func, double a, double b, int max, double err)
         {
             fa = f2(a);
         }
-        //Print stuff
+        
+        //Print data
+        cout << right << setw(3) << i << "  ";
+        cout << right << setfill(' ') << setw(6) << fixed << showpoint << setprecision(4) << a << "  ";
+        cout << right << setw(8) << fixed << showpoint << setprecision(4) << fa << "  ";
+        cout << right << setw(9) << setprecision(10) << abs(d) << endl;
     }
+    cout << "Max iterations reached. Either the function diverges or converges very slowly at these points\n";
 }
